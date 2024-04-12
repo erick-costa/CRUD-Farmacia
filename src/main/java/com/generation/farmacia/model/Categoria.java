47ac25bd8c1a76ce0feba27402faa11e6b9a6274
erick-costa <erickcosta2509@gmail.com>
2024-04-12 13:27:21 -0300

    implementação do CRUD de categorias

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,40 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O campo Nome é Obrigatório")
+	@Size(min = 5, max = 100, message = "Tamanho mínimo: 5, Tamano máximo: 100")
+	@Column(unique = true)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
